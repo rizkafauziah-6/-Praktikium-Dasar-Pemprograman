d09ea6a7a0a0ab0f37124afdd189dbfd0d8c057b
rizkafauziah-6 <145301198+rizkafauziah-6@users.noreply.github.com>
2023-09-27 23:38:59 +0700

    Add files via upload

---
+++ TugasRizka.java
@@ -0,0 +1,43 @@
+public class TugasRizka {
+public static void main (String[] args){
+    int A = 3, B = 6, C = 2, K =5, L = 4, M = 3;
+ // soal 1 
+// bagian a
+System.out.println(( 4 + 2 > A ) && ( B - 2 > 3 + 2) || (B + 2 <= 6 + 2));
+
+// bagian b
+System.out.println(( K + 5 < M ) || (C*M < L && 2*K-L > 0));
+
+// bagian c 
+System.out.println(( L + 5 < M ) || ( C*K < L && 2*K - L>0));
+
+// bagian d 
+System.out.println (( A * 4 <= 3*M +B ));
+
+// bagian e 
+System.out.println (( K+10 > A) && (L- 2>4 *C));
+
+
+// soal 2 hukum phytagoras
+import java.util.Scanner;
+Scanner toeremap = new Scanner(System.in);
+int a,b,c;
+System.out.print("Hitung segitiga siku -siku ");
+int a = angka.nextInt();
+System.out.println("Hitung segitiga siku -siku = + a ");
+// input nilai b
+System.out.print("nilai b:");
+int b = angka.nextInt();
+System.out.println("angka= + b");
+
+int b = angka.nextInt();
+System.out.println("masukkan nilai b =" + nilai b :");
+System.out.print("masukkan nilai b ");
+int a = angka.nextInt();
+System.out.println("\n");
+
+
+        
+    }
+}
+  
